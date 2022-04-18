ed3704bd91298dcbfe167781932b68e4c33f4e0d
HumanDuck23 <44237686+HumanDuck23@users.noreply.github.com>
2022-04-18 22:43:32 +0200

    created Logger.ts

---
+++ proxy/src/utils/Logger.ts
@@ -0,0 +1,60 @@
+import chalk from "chalk"
+
+/**
+ * Logger Utilities
+ */
+export class Logger {
+
+    static infoColor: string = "#498bf5"
+    static warnColor: string = "#f5d849"
+    static errorColor: string = "#eb6538"
+    static debugColor: string = "#8938eb"
+    static startupColor: string = "#ebe238"
+
+    /**
+     * Log an info message
+     * @param message
+     */
+    static info(message: string) {
+        const t = chalk.hex(this.infoColor)(`[${new Date().toISOString().split("T")[1]}] info | ${chalk.white(message)}`)
+        console.log(chalk.bold(t))
+    }
+
+    /**
+     * Log a warning message
+     * @param message
+     */
+    static warn(message: string) {
+        const t = chalk.hex(this.warnColor)(`[${new Date().toISOString().split("T")[1]}] warn | ${chalk.white(message)}`)
+        console.log(chalk.bold(t))
+    }
+
+
+    /**
+     * Log an error message
+     * @param message
+     */
+    static error(message: string) {
+        const t = chalk.hex(this.errorColor)(`[${new Date().toISOString().split("T")[1]}]error | ${chalk.white(message)}`)
+        console.log(chalk.bold(t))
+    }
+
+    /**
+     * Log a debug message
+     * @param message
+     */
+    static debug(message: string) {
+        const t = chalk.hex(this.debugColor)(`[${new Date().toISOString().split("T")[1]}]debug | ${chalk.white(message)}`)
+        console.log(chalk.bold(t))
+    }
+
+    /**
+     * Log a startup message
+     * @param message
+     */
+    static startup(message: string) {
+        const t = chalk.hex(this.startupColor)(`[${new Date().toISOString().split("T")[1]}]startup | ${chalk.white(message)}`)
+        console.log(chalk.bold(t))
+    }
+
+}
