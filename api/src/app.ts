2dc15224fdc7044792cb21cb2f63618243dd452f
HumanDuck23 <44237686+HumanDuck23@users.noreply.github.com>
2022-04-18 10:41:24 +0200

    created api

---
+++ api/src/app.ts
@@ -0,0 +1,12 @@
+const express = require("express")
+const app = express()
+
+const PORT = 4505
+
+app.get("/latest", (req: any, res: any) => {
+    res.sendFile(__dirname + "/latest.json")
+})
+
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`)
+})
