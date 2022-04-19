df2b479690f02f680e21de350210447df3fe7962
HumanDuck23 <44237686+HumanDuck23@users.noreply.github.com>
2022-04-19 08:45:48 +0200

    check if log file exists in main.ts

--- proxy/src/main.ts
+++ proxy/src/main.ts
@@ -1,6 +1,11 @@
 import { VirtualHypixel } from "./VirtualHypixel"
 import * as fs from "fs"
+import { Logger } from "./utils/Logger"
 
+if (!fs.existsSync("./vh-config.json")) {
+    Logger.error("Config file not found!")
+    process.exit(1)
+}
 const config = JSON.parse(fs.readFileSync("./vh-config.json", "utf8"))
 const vh = new VirtualHypixel()
 vh.start(config)
