4b696291ac28582b90cc5292843a499249496228
HumanDuck23 <44237686+HumanDuck23@users.noreply.github.com>
2022-04-19 22:56:52 +0200

    Modules.vue snackbar error to set path

--- launcher/src/views/Modules.vue
+++ launcher/src/views/Modules.vue
@@ -1,10 +1,36 @@
 <template>
-  <div>Modules</div>
+  <div>
+    <v-snackbar absolute bottom left v-model="pathSnack" color="error darken-2" :timeout="20000">
+      <v-icon class="mr-2">
+        mdi-alert-decagram-outline
+      </v-icon>
+      Please set the module folder in the settings!
+      <v-btn text class="ml-2" to="/settings">
+        <v-icon class="mr-2">mdi-open-in-new</v-icon>
+        Open
+      </v-btn>
+    </v-snackbar>
+  </div>
 </template>
 
 <script>
 export default {
-  name: "Modules"
+  name: "Modules",
+
+  data: () => ({
+    modules: [],
+    moduleConfigs: {},
+
+    pathSnack: false
+  }),
+
+  mounted() {
+    this.moduleConfigs = JSON.parse(localStorage.getItem("moduleConfigs")) || {}
+    const modulePath = JSON.parse(localStorage.getItem("settings"))?.modules?.path || ""
+    if (!modulePath) {
+      this.pathSnack = true
+    }
+  }
 }
 </script>
 
