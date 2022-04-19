30099adad24d807ad8d57f9b2b15da772821095d
HumanDuck23 <44237686+HumanDuck23@users.noreply.github.com>
2022-04-19 23:17:26 +0200

    Settings.vue module path setting

--- launcher/src/views/Settings.vue
+++ launcher/src/views/Settings.vue
@@ -1,12 +1,49 @@
 <template>
   <div>
-    Settings
+    <v-card class="ma-5" color="primary">
+      <v-card-title>Settings</v-card-title>
+      <v-card-text>
+        <v-text-field outlined @click:prepend-inner="pickFolder" label="Module Folder" prepend-inner-icon="mdi-folder-open-outline" v-model="settings.modulePath" color="accent"/>
+      </v-card-text>
+    </v-card>
   </div>
 </template>
 
 <script>
 export default {
-  name: "Settings"
+  name: "Settings",
+
+  data: () => ({
+    settings: {
+      modulePath: ""
+    }
+  }),
+
+  mounted() {
+    if (localStorage.getItem("settings")) {
+      this.settings = JSON.parse(localStorage.getItem("settings"))
+    }
+  },
+
+  methods: {
+    pickFolder() {
+      if (window.windowAPI) {
+        window.windowAPI.folderDialog()
+          .then(folder => {
+            this.settings.modulePath = folder[0]
+          })
+      }
+    }
+  },
+
+  watch: {
+    settings: {
+      handler: function(val) {
+        localStorage.setItem("settings", JSON.stringify(val))
+      },
+      deep: true
+    }
+  }
 }
 </script>
 
