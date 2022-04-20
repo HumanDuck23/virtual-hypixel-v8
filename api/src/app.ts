385e16ce6e7207d886db9a832097b2fdb4cf707e
HumanDuck23 <44237686+HumanDuck23@users.noreply.github.com>
2022-04-20 20:02:12 +0200

    API cors header

--- api/src/app.ts
+++ api/src/app.ts
@@ -3,6 +3,11 @@ const app = express()
 
 const PORT = 4505
 
+app.use(function (req: any, res: any, next: any) {
+    res.setHeader("Access-Control-Allow-Origin", "*")
+    next()
+})
+
 app.get("/latest", (req: any, res: any) => {
     res.sendFile(__dirname + "/latest.json")
 })
